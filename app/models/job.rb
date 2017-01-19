d25a04484692c7aec9688f7bc9d3dc61992e1b9f
Steve Licata <Steve.Licata@gmail.com>
2017-01-19 16:16:50 -0500

    changes

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,6 @@ class Job < ApplicationRecord
 
   has_and_belongs_to_many :boats
 
-  validates :name, length: { minimum: 100 }
+  validates :description, length: { minimum: 100 }
   validates :cost, numericality: { greater_than_or_equal_to: 1000 }
 end

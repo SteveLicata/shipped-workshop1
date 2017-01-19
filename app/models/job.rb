03c8cab20e3ba72a2e3460fd54099a39d3d85192
Matt Kim <matt.kim712@gmail.com>
2017-01-19 16:15:07 -0500

    new stufF

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,9 @@ class Job < ApplicationRecord
 
   has_and_belongs_to_many :boats
 
-  validates :name, length: { minimum: 100 }
+
+  validates :name, uniqueness: true
+
+  validates :description, length: { minimum: 100 }
   validates :cost, numericality: { greater_than_or_equal_to: 1000 }
 end
